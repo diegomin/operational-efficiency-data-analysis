d7122aa544be6f6a7a65e6d6a4ca27ea0092f82f
diegomin <minguraerivesdiego@gmail.com>
2026-01-20 19:55:42 -0600

    Add SQL query for sales analysis by category
    Added a basic SQL query to analyze sales by category.

--- sql-business-analytics/sales/basic_sales.sql
+++ sql-business-analytics/sales/basic_sales.sql
@@ -1,1 +1,12 @@
-
+--1. SQL BÁSICO ORIENTADO A NEGOCIO (VENTAS)
+--Pregunta de negocio/
+--¿Cuánto estamos vendiendo por categoría?
+SELECT 
+    p.category,
+    SUM(s.total_amount) AS total_sales
+FROM sales s
+JOIN products p ON s.product_id = p.product_id
+GROUP BY p.category
+ORDER BY total_sales DESC;
+--Explicación (para empresa)
+--Esta consulta permite identificar qué categorías generan mayor ingreso, información clave para decisiones de marketing, inventario y promociones.
