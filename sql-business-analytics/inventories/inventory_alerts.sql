85c9d3139ce45012e6c46654c55dc8e1fdc83c06
diegomin <minguraerivesdiego@gmail.com>
2026-01-20 20:45:23 -0600

    Add inventory alert query for low stock products
    Added SQL query to identify products at risk of stockouts.

--- sql-business-analytics/inventories/inventory_alerts.sql
+++ sql-business-analytics/inventories/inventory_alerts.sql
@@ -1,1 +1,12 @@
-
+--Pregunta
+--¿Qué productos están en riesgo de desabasto?
+SELECT 
+    p.product_name,
+    i.stock,
+    i.warehouse
+FROM inventory i
+JOIN products p ON i.product_id = p.product_id
+WHERE i.stock < 20
+ORDER BY i.stock ASC;
+--Decisión
+--Permite generar alertas tempranas para reabastecimiento.
