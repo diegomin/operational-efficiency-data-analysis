9666fa3f1fd3de39f6fa3f99c33bdd8bafa14694
diegomin <minguraerivesdiego@gmail.com>
2026-01-20 20:47:42 -0600

    Add query for average delivery time by status
    Added SQL query to calculate average delivery time by status.

--- sql-business-analytics/operations/kpis_operations.sql
+++ sql-business-analytics/operations/kpis_operations.sql
@@ -1,1 +1,9 @@
-
+--Pregunta
+--¿Tiempo promedio de entrega por estado?
+SELECT 
+    status,
+    AVG(delivery_time_days) AS avg_delivery_time
+FROM operations
+GROUP BY status;
+--Impacto
+--Ayuda a detectar cuellos de botella operativos.
